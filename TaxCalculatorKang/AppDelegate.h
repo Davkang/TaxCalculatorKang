c0759a1732ad5179c3a9e36ffded67ba2fd830ce
David Kang <kdragom@davids-mbp>
2016-01-09 15:43:47 -0800

    Initial Commit

---
+++ TaxCalculatorKang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculatorKang
+//
+//  Created by David Kang on 1/9/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalculatorKang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculatorKang
+//
+//  Created by David Kang on 1/9/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
